d68d63a7347958cffc8c0f73215e1f192fc5c5cd
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2026-05-11 10:53:23 +0200

    added sources.cmake

---
+++ src/util/sources.cmake
@@ -0,0 +1,10 @@
+
+# author        Oliver Blaser
+# date          11.05.2026
+# copyright     MIT - Copyright (c) 2026 Oliver Blaser
+
+set(UTIL_SOURCES
+${CMAKE_CURRENT_LIST_DIR}/string.c
+${CMAKE_CURRENT_LIST_DIR}/time.c
+${CMAKE_CURRENT_LIST_DIR}/version.c
+)
